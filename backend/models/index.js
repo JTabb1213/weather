20c8b1dcbfbcda77c696b8d3a14037351446f6b5
Jack Tabb <jacktabb@Jacks-MacBook-Pro-3.local>
2024-10-07 14:20:01 -0500

    database

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,15 +2,15 @@ const config = require('config');
 const Sequelize = require("sequelize");
 const pool = config.get('database.pool');
 
-const dbName = 'databaseforweatherwebsite';//config.get('database.db');
-const host = 'ldpg-crth9d3tq21c73aojapg-a.ohio-postgres.render.com';//config.get('database.host');
-const port = '5432';//config.get('database.port');
+const dbName = config.get('database.db');
+const host = config.get('database.host');
+const port = config.get('database.port');
 const dialect = config.get('database.dialect');
 console.log(`Using db: ${dialect}://${host}:${port}/${dbName}`);
 const sequelize = new Sequelize(
     dbName,
-    'databaseforweatherwebsite_user',//config.get('database.user'),
-    'a02fUuDkcM3OLItU4Rk8jlkydnqZYtDG',//config.get('database.password'),
+    config.get('database.user'),
+    config.get('database.password'),
     {
         host: host,
         port: port,
